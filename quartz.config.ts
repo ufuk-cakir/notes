5ac3a42e04edc003c2e13e0edd2920c74906164d
Ufuk Çakır <ufukcakir2001@gmail.com>
2024-10-31 14:26:55 +0000

    Quartz sync: Oct 31, 2024, 2:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-6HK6ZH308Y",
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
